a13c9f2785b7b08d6fd4b303b885bdbd4e0af232
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2023-09-09 14:51:31 +0800

    finished fakey

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-import "./App.css"
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [answer, setAnswer] = useState<string>("");
+
   return (
     <div className="App">
-      Hello World
+      <div className="title">
+        <b>Fakey</b>, the{" "}
+        <i className="side-padding">only</i> news detector
+        you need
+      </div>
+      <div
+        className={
+          answer === "True"
+            ? "answer correct"
+            : answer === "Loading..."
+            ? "answer loading"
+            : "answer wrong"
+        }
+      >
+        {answer}
+      </div>
+      <div
+        className="submit-bottom"
+        onClick={async () => {
+          setAnswer("Loading...");
+          await new Promise((res) => setTimeout(res, 1000));
+          setAnswer("True");
+        }}
+        onContextMenu={async (e) => {
+          e.preventDefault();
+          setAnswer("Loading...");
+          await new Promise((res) => setTimeout(res, 1000));
+          setAnswer("False");
+        }}
+      >
+        Detect
+      </div>
     </div>
   );
 }
